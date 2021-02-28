5d4913a642598ae5684c4e004fdbcb92a6e8114d
atanasster <atanasster@gmail.com>
2021-02-28 17:33:10 -0500

    update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,15 @@
-const path = require("path");
-const { defaultCompileProps } = require("@component-controls/core");
-const { getBundleName } = require("@component-controls/core/node-utils");
-
-const { compile, watch } = require("@component-controls/webpack-compile");
-
-const { resolve } = require("path");
+const path, { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const {
-  postBuild,
+  withComponentControls,
 } = require("@component-controls/react-router-integration/post-build");
 
 const isProd = process.env.NODE_ENV === "production";
 
 const outFolder = process.env.BUILD_PATH || "build";
 
-const buildOptions = {
-  ...defaultCompileProps,
-  configPath: ".config",
-  ...{
-    distFolder: process.env.DIST_PATH || path.join(process.cwd(), outFolder),
-    staticFolder:
-      process.env.STATIC_PATH || path.join(process.cwd(), outFolder, "static"),
-  },
-};
+const distFoloder = resolve(__dirname, outFolder);
 const config = {
   mode: isProd ? "production" : "development",
   entry: {
@@ -44,15 +30,6 @@ const config = {
         use: "babel-loader",
         exclude: /node_modules/,
       },
-      {
-        test: require.resolve("@component-controls/store/controls-store"),
-        use: {
-          loader: require.resolve("@component-controls/store/loader.js"),
-          options: {
-            bundleFileName: getBundleName(buildOptions),
-          },
-        },
-      },
     ],
   },
   plugins: [
@@ -70,7 +47,7 @@ if (isProd) {
 } else {
   // for more information, see https://webpack.js.org/configuration/dev-server
   config.devServer = {
-    contentBase: __dirname + "/build/",
+    contentBase: distFoloder,
     port: 8080,
     open: true,
     hot: true,
@@ -80,11 +57,8 @@ if (isProd) {
   };
 }
 
-module.exports = async function() {
-  const onBundle = async ({ store }) => {
-    await postBuild(buildOptions.staticFolder, store);
-  };
-  const run = process.env.NODE_ENV === "development" ? watch : compile;
-  await run(buildOptions, onBundle);
-  return config;
-};
+module.exports = withComponentControls({
+  config,
+  development: !isProd,
+  options: { distFolder },
+});
