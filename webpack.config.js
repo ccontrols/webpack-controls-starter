b71b281e89080c0aa81dae5999e9944053f62e08
atanasster <atanasster@gmail.com>
2021-02-28 17:36:30 -0500

    update weback config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path, { resolve } = require("path");
+const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
 const {
