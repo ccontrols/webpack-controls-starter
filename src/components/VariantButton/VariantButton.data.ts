c87e6789a7b84ce32ccb846b74d51132065ae751
atanasster <atanasster@gmail.com>
2021-05-09 07:07:19 +0300

    data data-driven testing

---
+++ src/components/VariantButton/VariantButton.data.ts
@@ -0,0 +1,24 @@
+export default {
+  overview: {
+    "0": {
+      text: "Edwin Kutch",
+      icon: "Floy Hermann"
+    },
+    "1": {
+      text: "Autumn Bernhard",
+      icon: "Krystel McDermott III"
+    },
+    "2": {
+      text: "Aliya Kemmer",
+      icon: "Elva Hane"
+    },
+    "3": {
+      text: "London Hudson",
+      icon: "Heaven Zulauf"
+    },
+    "4": {
+      text: "Leonard Bode",
+      icon: "Elisa Altenwerth"
+    }
+  }
+};
